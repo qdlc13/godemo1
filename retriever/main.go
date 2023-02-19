f20b76002a832e0da9c771cd85e2d121838cae25
fengqing <qdlc13@gmail.com>
2023-02-19 14:44:40 +0800

    23.02.19.01

--- retriever/main.go
+++ retriever/main.go
@@ -7,18 +7,44 @@ import (
 	"time"
 )
 
+const url = "http://www.baidu.com"
+
 type Retriever interface {
 	Get(url string) string
 }
 
 func download(r Retriever) string {
-	return r.Get("https://www.baidu.com")
+	return r.Get(url)
 }
+
+// 接口的组合
+
+type Poster interface {
+	Post(url string, form map[string]string) string
+}
+
+// 组合
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func session(s RetrieverPoster) string {
+
+	s.Post(url, map[string]string{
+		"contents": "another faked baidu.com",
+	})
+
+	return s.Get(url)
+}
+
 func main() {
 	//r不是简单的引用 有类型%T和值%v
 	var r Retriever
+	retriever := mock.Retriever{"This is a fake baidu.com"}
+	r = &retriever
 	//r = &mock.Retriever{"This is a fake baidu.com"}也可以
-	r = mock.Retriever{"This is a fake baidu.com"}
+	//r = mock.Retriever{"This is a fake baidu.com"}
 	fmt.Println(download(r))
 	inspect(r)
 
@@ -30,28 +56,34 @@ func main() {
 
 	//判断类型的等价写法
 	//Type Assertion
-	realRetriever := r.(*real.Retriever)
-	fmt.Println(realRetriever.TimeOut) //1m0s
+	realRetriever := r.(*real.Retriever) //判断是不是realRetriever 是的话把值赋给前面
+	fmt.Println(realRetriever.TimeOut)   //1m0s
 
-	if mockRetriever, ok := r.(mock.Retriever); ok {
+	if mockRetriever, ok := r.(*mock.Retriever); ok {
 		fmt.Println(mockRetriever.Contents)
 	} else {
 		fmt.Println("not a mock retriever")
 	}
 
-	//	r = &test.Rr{"this is a test"}
-	//	fmt.Printf("%T %v\n", r, r)
-	//	fmt.Println(download(r))
+	//r = &test.Rr{"this is a test"}
+	//fmt.Printf("%T %v\n", r, r)
 	//fmt.Println(download(r))
+	//fmt.Println(download(r))
+	//r = &mock.Retriever{"This is a fake baidu.com"}
+	fmt.Println("Try a session")
+	fmt.Println(session(&retriever)) //不能传r因为r只有Retriever的能力缺少方法的实现
 }
+
 func inspect(r Retriever) {
-	fmt.Printf("%T %v\n", r, r)
+	fmt.Println("Inspecting", r)
+	fmt.Printf(" > %T %v\n", r, r)
+	fmt.Print(" > Type switch:")
 	//Type Switch 判断类型
 	switch v := r.(type) {
-	case mock.Retriever:
+	case *mock.Retriever:
 		fmt.Println("Content:", v.Contents)
 	case *real.Retriever:
 		fmt.Println("UserAgent:", v.UserAgent)
-
 	}
+	fmt.Println()
 }
--- retriever/mock/mockRetriever.go
+++ retriever/mock/mockRetriever.go
@@ -1,12 +1,23 @@
 package mock
 
+import "fmt"
+
 type Retriever struct {
 	Contents string
 }
 
+// 类似java tostring
+func (r *Retriever) String() string {
+	return fmt.Sprintf("Retriever:{Contents=%s}", r.Contents)
+}
+
 // 值接收者
-func (r Retriever) Get(url string) string {
-	//TODO implement me
+func (r *Retriever) Get(url string) string {
 	return r.Contents
-	panic("implement me")
+}
+
+// 实现Poster接口
+func (r *Retriever) Post(url string, form map[string]string) string {
+	r.Contents = form["contents"]
+	return "ok"
 }
